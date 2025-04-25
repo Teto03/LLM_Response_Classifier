df06030482b59c1cab7621e5378ee7787119b21f
Teto03 <bianchi.teto@gmail.com>
2025-04-25 17:51:03 +0200

    addsddd

--- DATASETS/cartella test valutatore/delabler.py
+++ DATASETS/cartella test valutatore/delabler.py
@@ -1,7 +1,7 @@
 import json
 
 # Leggi il file di input
-with open('response.json', 'r', encoding='utf-8') as file:
+with open('te.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Rimuovi solo le etichette da ogni oggetto, mantenendo le risposte
@@ -11,7 +11,7 @@ for item in data:
         responses_without_labels.append({"response": item["response"]})
 
 # Scrivi i dati modificati in un nuovo file JSON
-with open('responses_without_labels.json', 'w', encoding='utf-8') as outfile:
+with open('Test12.json', 'w', encoding='utf-8') as outfile:
     json.dump(responses_without_labels, outfile, ensure_ascii=False, indent=2)
 
 print("File creato con successo: responses_without_labels.json")
